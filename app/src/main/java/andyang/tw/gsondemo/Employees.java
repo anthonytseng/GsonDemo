32057c32ace9b38de4bb8d0fae53a1ea5c1f4a23
bng86 <yang.bng86@gmail.com>
2014-12-20 16:08:58 +0800

    init commit

---
+++ app/src/main/java/andyang/tw/gsondemo/Employees.java
@@ -0,0 +1,28 @@
+package andyang.tw.gsondemo;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by andyang on 14/12/20.
+ */
+public class Employees {
+
+    @SerializedName("firstName") String firstName;
+    @SerializedName("lastName") String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ app/src/main/java/andyang/tw/gsondemo/MainActivity.java
@@ -0,0 +1,47 @@
+package andyang.tw.gsondemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.io.InputStream;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ListView lv;
+    private ListAdapter adapter;
+    private List<Employees> employeeses;
+    private EmployeesParser parser;
+    private String TAG = MainActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.lv);
+        String json = getAssetJson();
+        parser = new EmployeesParser();
+        employeeses = parser.jsonToObject(json);
+        adapter = new ListAdapter(getApplicationContext(),employeeses);
+        lv.setAdapter(adapter);
+    }
+
+    public String getAssetJson(){
+        try {
+            InputStream stream = getAssets().open("employeesJson.json");
+
+            int size = stream.available();
+            byte[] buffer = new byte[size];
+            stream.read(buffer);
+            stream.close();
+            String json = new String(buffer);
+            return  json;
+        } catch (Exception e){
+            Log.e(TAG, e.toString());
+        }
+        return "";
+    }
+}
